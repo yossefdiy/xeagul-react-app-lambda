37e0bacff6f15b3897d52450690bb1d7f0c6843d
diy <yossefdiy@gmail.com>
2022-02-11 19:43:47 +0200

    grid sample

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import "./App.css" ;
 
 
-
-export default function App() {
+ function App() {
   return (
     <div className='App'>
-        <div className='game'>tic tac toe 11</div>
-    </div>
+    <h1>TIC TAC TOE</h1>
+     
+<div class="grid-container">
+  <div class="grid-item1">X</div>
+  <div class="grid-item2">X</div>
+  <div class="grid-item3">X</div>  
+  <div class="grid-item4">O</div>
+  <div class="grid-item5">O</div>
+  <div class="grid-item6">O</div>  
+  <div class="grid-item7">O</div>
+  <div class="grid-item8">O</div>
+  <div class="grid-item9">O</div>  
+</div>
+</div>
   )
 }
+export default App

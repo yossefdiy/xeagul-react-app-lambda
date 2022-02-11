d8ffe2ea3930b2186babe99c98aee34f0872a09e
diy <yossefdiy@gmail.com>
2022-02-11 14:37:47 +0200

    s

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default function App() {
   return (
     <div className='App'>
-        <div className='game'>tic tac toe</div>
+        <div className='game'>tic tac toe 11</div>
     </div>
   )
 }

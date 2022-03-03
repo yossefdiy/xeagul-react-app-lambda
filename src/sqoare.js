c3d876969c8388afb81a4355e78c127d8132002c
diy <yossefdiy@gmail.com>
2022-03-03 13:41:33 +0200

    FINISH THE GAME

---
+++ src/sqoare.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Square(props){
+    return(
+      <div>
+        <button className="bt" onClick={()=> props.onClick()}>{props.value}</button>
+      </div>
+    )
+  }
+    export default Square
+  
